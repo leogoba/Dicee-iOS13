2b48bfcea77184b0f8e84a45ac858c5951cf6b21
leogoba <leogobamusic@yandex.ru>
2023-01-18 21:23:12 +0400

    Bug fix, cleaning code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,20 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet var diceImageView1: UIImageView!
     @IBOutlet var diceImageView2: UIImageView!
     
-    let array = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-    var count1 = 1
-    var count2 = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
-        diceImageView1.image = array[count1]
-        count1 += 1
-        
-        diceImageView2.image = array[count2]
-        count2 -= 1
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
     }
 }
